d2fba15d20de99f73a3be830d2c62818080d6656
kiavash-jafari <kiavashjafari2020@gmail.com>
2025-02-17 07:57:46 -0800

    Update hangri python.py

--- hangri python.py
+++ hangri python.py
@@ -4,13 +4,15 @@ my_dict = [
         "name": "mamadshop",
         "goods": ["milk","salt","hobobat","kiksmaks"] 
     },
-    {  
+    { 
         "name": "asgarshop",
-        "goods": ["kimdi","felfel","tkhmemorg"]
-    }
+        "goods": ["kimdi","felfel","tkhmemorg","asal"]
+    },
     {
         "name": "abbasshop",
         "goods": ["chebs","pofak","hobobat","kopol"] 
     },
 ]
+
+a = input("serch your cala")
 
