1f6ce4f2b49ec9d5cb6ad7e9f4c3bd003921cce9
kiavash-jafari <kiavashjafari2020@gmail.com>
2025-02-18 10:43:59 -0800

    Update hangri python.py

--- hangri python.py
+++ hangri python.py
@@ -1,18 +1,27 @@
-my_dict = {}
-my_dict = [
+supermarket = {}
+supermarket = [
     {
         "name": "mamadshop",
-        "goods": ["milk","salt","hobobat","kiksmaks"] 
+        "goods": ["milk__35000t","salt__10000t","hobobat__20000t","kiksmaks__34000t"] 
     },
     { 
         "name": "asgarshop",
-        "goods": ["kimdi","felfel","tkhmemorg","asal"]
+        "goods": ["kimdi__22000t","felfel__10000t","tkhmemorg__40000t","asal__100000t"]
     },
     {
         "name": "abbasshop",
-        "goods": ["chebs","pofak","hobobat","kopol"] 
+        "goods": ["cheb__65000t","pofak__45000t","hobobat__30000t","kopol__5000t"] 
     },
 ]
 
-a = input("serch your cala")
 
+def search_items ():
+    a = input("serch your cala")
+    shop = "mamadshop","asgarshop","abbasshop"
+    mahsol = "milk","salt","hobobat","kiksmaks","kimdi","felfel","tkhmemorg","asal","chebs","pofak","hobobat","kopol"
+    if mahsol in shop ():
+        if item in supermarket[mahsol]:
+            print('your mahsol is in len(shop)')
+        elif():
+            print('your mahsol peyda nasod')
+              
